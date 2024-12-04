6a69a53be5bffa44a3b52db9f5f8c06188601384
Jana Schueler <janaschueler@protonmail.com>
2024-12-04 12:14:55 +0100

    First Comit

---
+++ tic-tac-toe/scripts/db.js
@@ -0,0 +1,9 @@
+let starter = [
+  {
+    dishName: "Samosa",
+    dishAmount: "(2 pieces)",
+    dishDiscription: "Pastry filled with a mixture of potatoes, green peas, cashews, and spices.",
+    dishprice: 4.99,
+    amount: 0,
+  },
+];
